ec8162b35bf87554484bb29f8d83cb808380766c
SokolovaChristina <homeusechrist0@gmail.com>
2025-10-19 17:15:47 +0500

    Task1

---
+++ Tyuiu.SokolovaHS.Sprint3.Task1.V2.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SokolovaHS.Sprint3.Task1.V2.Lib
+{
+    public class DataService : ISprint3Task1V2
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double sum = 0;
+            int i = startValue;
+            while (i <= stopValue)
+            {
+                sum += Math.Sin(i) * 0.5;
+                i++;
+            }
+            return Math.Round(sum, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint3.Task1.V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaHS.Sprint3.Task1.V2.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint3.Task1.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Соколова Х. С. | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Соколова Христина Сергеевна | ПКТБ-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда   *");
+            Console.WriteLine("* по формуле                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double result = ds.GetSumSeries(startValue, stopValue);
+            Console.WriteLine($"Сумма ряда = {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
